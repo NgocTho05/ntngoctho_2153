a3b50bebaf72c44f5a70505031b626ac6828c850
ngoc tho <nguyenthingoctho10932@gmail.com>
2026-06-03 09:45:31 +0700

    hoan thanh bai rsa

--- lab03/cipher/rsa/rsa_cipher.py
+++ lab03/cipher/rsa/rsa_cipher.py
@@ -1,45 +1,40 @@
-import rsa
-import os
+import rsa, os
 
-KEYS_DIR = os.path.join(os.path.dirname(__file__), 'keys')
-
+if not os.path.exists('cipher/rsa/keys'):
+    os.makedirs('cipher/rsa/keys')
 
 class RSACipher:
     def __init__(self):
-        os.makedirs(KEYS_DIR, exist_ok=True)
+        pass
 
-    def generate_keys(self, key_size=2048):
-        public_key, private_key = rsa.newkeys(key_size)
-        with open(os.path.join(KEYS_DIR, 'public.pem'), 'wb') as f:
-            f.write(public_key.save_pkcs1())
-        with open(os.path.join(KEYS_DIR, 'private.pem'), 'wb') as f:
-            f.write(private_key.save_pkcs1())
+    def generate_keys(self):
+        (public_key, private_key) = rsa.newkeys(1024)
+        with open('cipher/rsa/keys/publicKey.pem', 'wb') as p:
+            p.write(public_key.save_pkcs1('PEM'))
+        with open('cipher/rsa/keys/privateKey.pem', 'wb') as p:
+            p.write(private_key.save_pkcs1('PEM'))
 
     def load_keys(self):
-        with open(os.path.join(KEYS_DIR, 'private.pem'), 'rb') as f:
-            private_key = rsa.PrivateKey.load_pkcs1(f.read())
-        with open(os.path.join(KEYS_DIR, 'public.pem'), 'rb') as f:
-            public_key = rsa.PublicKey.load_pkcs1(f.read())
+        with open('cipher/rsa/keys/publicKey.pem', 'rb') as p:
+            public_key = rsa.PublicKey.load_pkcs1(p.read())
+        with open('cipher/rsa/keys/privateKey.pem', 'rb') as p:
+            private_key = rsa.PrivateKey.load_pkcs1(p.read())
         return private_key, public_key
 
     def encrypt(self, message, key):
-        if isinstance(message, str):
-            message = message.encode('utf-8')
-        return rsa.encrypt(message, key)
+        return rsa.encrypt(message.encode('ascii'), key)
 
     def decrypt(self, ciphertext, key):
-        return rsa.decrypt(ciphertext, key).decode('utf-8')
-
-    def sign(self, message, private_key):
-        if isinstance(message, str):
-            message = message.encode('utf-8')
-        return rsa.sign(message, private_key, 'SHA-256')
-
-    def verify(self, message, signature, public_key):
-        if isinstance(message, str):
-            message = message.encode('utf-8')
         try:
-            rsa.verify(message, signature, public_key)
-            return True
-        except rsa.VerificationError:
+            return rsa.decrypt(ciphertext, key).decode('ascii')
+        except:
+            return False
+
+    def sign(self, message, key):
+        return rsa.sign(message.encode('ascii'), key, 'SHA-1')
+
+    def verify(self, message, signature, key):
+        try:
+            return rsa.verify(message.encode('ascii'), signature, key,) == 'SHA-1'
+        except:
             return False
